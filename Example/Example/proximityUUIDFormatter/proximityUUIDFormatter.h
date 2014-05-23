7bb6a3713b8ece00ce6a339470dd44c4c6dfaa08
Jonathon Hibbard <jhibbard@gmail.com>
2014-05-23 13:48:57 -0400

    Update proximityUUIDFormatter.h

--- Example/Example/proximityUUIDFormatter/proximityUUIDFormatter.h
+++ Example/Example/proximityUUIDFormatter/proximityUUIDFormatter.h
@@ -8,13 +8,17 @@
 
 @interface proximityUUIDFormatter : NSObject
 
-// Is this a valid ProximityUUID?
-+(BOOL)isValidProximityUUID:(NSString *)proximityUUID;
+/**
+ * Some (extra) helpers...
+ */
++(NSString *)generateUUID;                              // Quick way to generate a random UUID
++(NSUUID *)proximityUUIDFromUUID:(id)UUID;              // Convenience method to allow us to work with either a UUID String or Object
++(BOOL)isValidProximityUUID:(NSString *)proximityUUID;  // Is this a valid ProximityUUID?
 
-// Ensures we keep a properly structured, 32-character value.
-+(void)storeValuesInTextField:(UITextField *)textField;
-
-// Handles the actual heavy lifting of formatting/keeping track of cursor position, and all that other jazz...
-+(void)formatTextField:(UITextField *)textField;
+/**
+ * Usage methods
+ */
++(void)storeValuesInTextField:(UITextField *)textField;  // Ensures we keep a properly structured, 32-character value.
++(void)formatTextField:(UITextField *)textField;         // Handles the actual heavy lifting of formatting/keeping track of cursor position, and all that other jazz...
 
 @end
